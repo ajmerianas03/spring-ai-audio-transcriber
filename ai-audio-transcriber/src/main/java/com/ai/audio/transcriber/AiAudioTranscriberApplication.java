2d742839193beb194d3c7cbf2e30e4a864b1b415
Anas Ajmeri <ajmerianas03@gmail.com>
2025-11-07 15:08:13 +0530

    first Phase completed

---
+++ ai-audio-transcriber/src/main/java/com/ai/audio/transcriber/AiAudioTranscriberApplication.java
@@ -0,0 +1,13 @@
+package com.ai.audio.transcriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAudioTranscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAudioTranscriberApplication.class, args);
+	}
+
+}
---
+++ ai-audio-transcriber/src/test/java/com/ai/audio/transcriber/AiAudioTranscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.audio.transcriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAudioTranscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
